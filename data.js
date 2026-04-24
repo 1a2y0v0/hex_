8a0af1a9e9c2856ed26493b8e168f16ec44f3ce6
1a2y0v0 <425513775@qq.com>
2026-04-24 23:02:56 +0800

    鏇存柊绉垎鏁版嵁

--- data.js
+++ data.js
@@ -1,20 +1,190 @@
-// 海克斯积分数据 - 每次更新只需修改此数组
+// 海克斯积分数据 - 自动生成于 2026/4/24 23:02:54
 const detailData = [
-    { name: '胆小菇(丁真)', winBonus: 23, loseBonus: 13, fire: 51, shield: 10, assist: 10, extra: 22, penalty: 0, total: 93 },
-    { name: '小姐(朱赛)', winBonus: 14, loseBonus: 15, fire: 18, shield: 18, assist: 22, extra: 23, penalty: -3, total: 78 },
-    { name: '香农(张中伟)', winBonus: 14, loseBonus: 15, fire: 11, shield: 14, assist: 27, extra: 21, penalty: 0, total: 73 },
-    { name: '品如(兰海)', winBonus: 14, loseBonus: 15, fire: 18, shield: 16, assist: 16, extra: 18, penalty: -4, total: 64 },
-    { name: '智神(向智豪)', winBonus: 11, loseBonus: 18, fire: 3, shield: 29, assist: 24, extra: 4, penalty: -3, total: 57 },
-    { name: '劳动人民(卓恒)', winBonus: 15, loseBonus: 9, fire: 24, shield: 5, assist: 11, extra: 13, penalty: -2, total: 51 },
-    { name: '一条柴(周延)', winBonus: 8, loseBonus: 14, fire: 5, shield: 20, assist: 20, extra: 6, penalty: -3, total: 48 },
-    { name: '我的哥(周春江)', winBonus: 10, loseBonus: 13, fire: 14, shield: 5, assist: 17, extra: 1, penalty: -3, total: 34 },
-    { name: 'MING(周涛)', winBonus: 8, loseBonus: 6, fire: 17, shield: 8, assist: 0, extra: 7, penalty: 0, total: 32 },
-    { name: '大喷菇(吴成玮)', winBonus: 6, loseBonus: 15, fire: 3, shield: 24, assist: 6, extra: 0, penalty: -2, total: 31 },
-    { name: '小楼(李青林)', winBonus: 8, loseBonus: 7, fire: 8, shield: 9, assist: 8, extra: 5, penalty: -2, total: 28 },
-    { name: '宝批龙(周志文)', winBonus: 6, loseBonus: 2, fire: 1, shield: 4, assist: 11, extra: 9, penalty: -2, total: 23 },
-    { name: '摸鱼(李世文)', winBonus: 6, loseBonus: 4, fire: 2, shield: 11, assist: 10, extra: 0, penalty: 0, total: 23 },
-    { name: '彭于晏(罗陈瑞)', winBonus: 3, loseBonus: 4, fire: 5, shield: 4, assist: 2, extra: 2, penalty: 0, total: 13 },
-    { name: '亏欠(姚康熙)', winBonus: 5, loseBonus: 2, fire: 3, shield: 4, assist: 5, extra: 0, penalty: -4, total: 8 },
-    { name: '茶百道(邝从海)', winBonus: 1, loseBonus: 4, fire: 0, shield: 2, assist: 6, extra: 0, penalty: -1, total: 7 },
-    { name: '阎王(陈龙)', winBonus: 2, loseBonus: 2, fire: 3, shield: 0, assist: 3, extra: 0, penalty: -1, total: 5 }
+    {
+        "name": "胆小菇（丁真）",
+        "winBonus": 23,
+        "loseBonus": 13,
+        "fire": 51,
+        "shield": 10,
+        "assist": 10,
+        "extra": 22,
+        "penalty": 0,
+        "total": 888
+    },
+    {
+        "name": "小姐（朱赛）",
+        "winBonus": 14,
+        "loseBonus": 15,
+        "fire": 18,
+        "shield": 18,
+        "assist": 22,
+        "extra": 23,
+        "penalty": -3,
+        "total": 78
+    },
+    {
+        "name": "香农（张中伟）",
+        "winBonus": 14,
+        "loseBonus": 15,
+        "fire": 11,
+        "shield": 14,
+        "assist": 27,
+        "extra": 21,
+        "penalty": 0,
+        "total": 73
+    },
+    {
+        "name": "品如（兰海）",
+        "winBonus": 14,
+        "loseBonus": 15,
+        "fire": 18,
+        "shield": 16,
+        "assist": 16,
+        "extra": 18,
+        "penalty": -4,
+        "total": 64
+    },
+    {
+        "name": "智神（向智豪）",
+        "winBonus": 11,
+        "loseBonus": 18,
+        "fire": 3,
+        "shield": 29,
+        "assist": 24,
+        "extra": 4,
+        "penalty": -3,
+        "total": 57
+    },
+    {
+        "name": "劳动人民(卓恒)",
+        "winBonus": 15,
+        "loseBonus": 9,
+        "fire": 24,
+        "shield": 5,
+        "assist": 11,
+        "extra": 13,
+        "penalty": -2,
+        "total": 51
+    },
+    {
+        "name": "一条柴（周延）",
+        "winBonus": 8,
+        "loseBonus": 14,
+        "fire": 5,
+        "shield": 20,
+        "assist": 20,
+        "extra": 6,
+        "penalty": -3,
+        "total": 48
+    },
+    {
+        "name": "我的哥（周春江）",
+        "winBonus": 10,
+        "loseBonus": 13,
+        "fire": 14,
+        "shield": 5,
+        "assist": 17,
+        "extra": 1,
+        "penalty": -3,
+        "total": 34
+    },
+    {
+        "name": "MING（周涛）",
+        "winBonus": 8,
+        "loseBonus": 6,
+        "fire": 17,
+        "shield": 8,
+        "assist": 0,
+        "extra": 7,
+        "penalty": 0,
+        "total": 32
+    },
+    {
+        "name": "大喷菇（吴成玮）",
+        "winBonus": 6,
+        "loseBonus": 15,
+        "fire": 3,
+        "shield": 24,
+        "assist": 6,
+        "extra": 0,
+        "penalty": -2,
+        "total": 31
+    },
+    {
+        "name": "小楼（李青林）",
+        "winBonus": 8,
+        "loseBonus": 7,
+        "fire": 8,
+        "shield": 9,
+        "assist": 8,
+        "extra": 5,
+        "penalty": -2,
+        "total": 28
+    },
+    {
+        "name": "宝批龙（周志文）",
+        "winBonus": 6,
+        "loseBonus": 2,
+        "fire": 1,
+        "shield": 4,
+        "assist": 11,
+        "extra": 9,
+        "penalty": -2,
+        "total": 23
+    },
+    {
+        "name": "摸鱼（李世文）",
+        "winBonus": 6,
+        "loseBonus": 4,
+        "fire": 2,
+        "shield": 11,
+        "assist": 10,
+        "extra": 0,
+        "penalty": 0,
+        "total": 23
+    },
+    {
+        "name": "彭于晏（罗陈瑞）",
+        "winBonus": 3,
+        "loseBonus": 4,
+        "fire": 5,
+        "shield": 4,
+        "assist": 2,
+        "extra": 2,
+        "penalty": 0,
+        "total": 13
+    },
+    {
+        "name": "亏欠（姚康熙）",
+        "winBonus": 5,
+        "loseBonus": 2,
+        "fire": 3,
+        "shield": 4,
+        "assist": 5,
+        "extra": 0,
+        "penalty": -4,
+        "total": 8
+    },
+    {
+        "name": "茶百道（邝从海）",
+        "winBonus": 1,
+        "loseBonus": 4,
+        "fire": 0,
+        "shield": 2,
+        "assist": 6,
+        "extra": 0,
+        "penalty": -1,
+        "total": 7
+    },
+    {
+        "name": "阎王（陈龙）",
+        "winBonus": 2,
+        "loseBonus": 2,
+        "fire": 3,
+        "shield": 0,
+        "assist": 3,
+        "extra": 0,
+        "penalty": -1,
+        "total": 5
+    }
 ];
